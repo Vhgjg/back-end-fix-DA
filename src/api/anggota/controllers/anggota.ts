aa8d957e6aec1e2ff9a93e57b882cdf50014c326
Vhgjg <rifqiaqmal5@gmail.com>
2026-04-04 19:24:16 +0700

    Initial commit: Strapi Backend Ambalan

---
+++ src/api/anggota/controllers/anggota.ts
@@ -0,0 +1,7 @@
+/**
+ * anggota controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::anggota.anggota');
---
+++ src/api/anggota/services/anggota.ts
@@ -0,0 +1,7 @@
+/**
+ * anggota service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::anggota.anggota');
